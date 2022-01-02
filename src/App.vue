139079895296326e9fe2a5bec43f3dee3068b47b
Alen <alechkr@gmail.com>
2022-01-02 13:28:32 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedRessources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'Official documentation of vue.js',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
