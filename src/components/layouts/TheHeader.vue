1ebc6960bdd8ebe5bd0c80281c2f1a12d2b6e833
Alen <alechkr@gmail.com>
2022-01-23 08:20:47 +0100

    style component title

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -9,4 +9,18 @@ export default {
   props: ['title'],
 };
 </script>
-<style lang="scss" scoped></style>
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #640032;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header h1 {
+  color: white;
+  margin: 0;
+}
+</style>
