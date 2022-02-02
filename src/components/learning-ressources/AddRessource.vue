45dc76d5d1bf926620cbe6d24cf1fb93d73a8d24
Alen <alechkr@gmail.com>
2022-02-02 09:15:29 +0100

    add form

--- src/components/learning-ressources/AddRessource.vue
+++ src/components/learning-ressources/AddRessource.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
+  <base-card>
     <h2>Add Ressource</h2>
+    <form>
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input type="text" id="title" name="title" />
+      </div>
 
-    The form will be shown here
-  </div>
+      <div class="form-control">
+        <label for="description">Description</label
+        ><textarea id="description" name="description" rows="3"></textarea>
+      </div>
+
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input type="url" id="link" name="link" />
+      </div>
+      <div><base-button type="submit" buttonTitle="Add Ressource" /></div>
+    </form>
+  </base-card>
 </template>
 
 <script>
@@ -11,7 +26,7 @@ export default {};
 </script>
 
 <style scoped>
-h2 {
-  background-color: red;
+.form-control {
+  background-color: lightgreen;
 }
 </style>
